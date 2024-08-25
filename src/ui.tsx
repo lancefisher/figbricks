6ab4ca1b7c43534e6220c2c93d365822fb02e177
Lance Fisher <lance@fisher.io>
2024-08-25 14:55:27 -0600

    Blockly rendering

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,4 +1,5 @@
 import '!prismjs/themes/prism.css'
+import { useBlocklyWorkspace } from 'react-blockly'
 
 import {
   Button,
@@ -9,61 +10,95 @@ import {
 import { emit } from '@create-figma-plugin/utilities'
 import { h, RefObject } from 'preact'
 import { useCallback, useEffect, useRef, useState } from 'preact/hooks'
-import { highlight, languages } from 'prismjs'
-import Editor from 'react-simple-code-editor'
+
 
 import styles from './styles.css'
-import { InsertCodeHandler } from './types'
 
 function Plugin() {
   const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`)
-  const containerElementRef : RefObject<HTMLDivElement> = useRef(null)
   const handleInsertCodeButtonClick = useCallback(
     function () {
-      emit<InsertCodeHandler>('INSERT_CODE', code)
+      console.log('todo')
+      //emit<InsertCodeHandler>('INSERT_CODE', code)
     },
     [code]
   )
-  // Patch to make `react-simple-code-editor` compatible with Preact
-  useEffect(function () {
-    const containerElement = containerElementRef.current
-    if (containerElement === null) {
-      return
+
+
+
+  
+  const initialXml =
+  '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>';
+
+  const toolboxCategories = {
+    kind: "categoryToolbox",
+    contents: [
+      {
+        kind: "category",
+        name: "Logic",
+        colour: "#5C81A6",
+        contents: [
+          {
+            kind: "block",
+            type: "controls_if",
+          },
+          {
+            kind: "block",
+            type: "logic_compare",
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "Math",
+        colour: "#5CA65C",
+        contents: [
+          {
+            kind: "block",
+            type: "math_round",
+          },
+          {
+            kind: "block",
+            type: "math_number",
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "Custom",
+        colour: "#5CA699",
+        contents: [
+          {
+            kind: "block",
+            type: "new_boundary_function",
+          },
+          {
+            kind: "block",
+            type: "return",
+          },
+        ],
+      },
+    ],
+  };
+
+  const blocklyRef = useRef(null);
+  const { workspace, xml } = useBlocklyWorkspace({
+    ref: blocklyRef,
+    toolboxConfiguration: toolboxCategories,
+    initialXml: initialXml,
+    workspaceConfiguration: {
+      grid: {
+        spacing: 20,
+        length: 3,
+        colour: "#ccc",
+        snap: true,
+      },
     }
-    const textAreaElement = containerElement.querySelector('textarea')
-    if (textAreaElement === null) {
-      return
-    }
-    textAreaElement.textContent = code
-    const preElement = containerElement.querySelector('pre')
-    if (preElement === null) {
-      return
-    }
-    if (textAreaElement.nextElementSibling !== preElement) {
-      textAreaElement.after(preElement)
-    }
-  }, [code])
+  });
+
+
   return (
-    <Container space="medium">
-      <VerticalSpace space="small" />
-      <h1>Lance was here</h1>
-      <div class={styles.container} ref={containerElementRef}>
-        <Editor
-          highlight={function (code: string) {
-            return highlight(code, languages.js, 'js')
-          }}
-          onValueChange={setCode}
-          preClassName={styles.editor}
-          textareaClassName={styles.editor}
-          value={code}
-        />
-      </div>
-      <VerticalSpace space="large" />
-      <Button fullWidth onClick={handleInsertCodeButtonClick}>
-        Insert Code
-      </Button>
-      <VerticalSpace space="small" />
-    </Container>
+    <div ref={blocklyRef} />
   )
 }
 
